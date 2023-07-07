8a34a437eb9fd73cf82a699d41222e5d0d87341a
Hankyul Min <denirokr22@gmail.com>
2023-07-07 13:43:52 +0900

    Init: content-slave-service 프로젝트

---
+++ src/main/java/com/travelvcommerce/contentslaveservice/ContentSlaveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travelvcommerce.contentslaveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSlaveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentSlaveServiceApplication.class, args);
+    }
+
+}
