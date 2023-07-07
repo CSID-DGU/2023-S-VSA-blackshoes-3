27795fecf499f0cfec1b8cfb9ae7e2ba1db825d8
Hankyul Min <denirokr22@gmail.com>
2023-07-07 13:38:16 +0900

    Init: statistics-service 프로젝트

---
+++ src/test/java/com/travelvcommerce/statisticsservice/StatisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelvcommerce.statisticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
