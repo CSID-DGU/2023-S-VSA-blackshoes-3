92273716528ccd800eaab17cec4f232bc1e55ec4
Hankyul Min <denirokr22@gmail.com>
2023-07-14 01:41:54 +0900

    Refactor: TagDto tag_id -> tagId

--- src/main/java/com/travelvcommerce/uploadservice/dto/TagDto.java
+++ src/main/java/com/travelvcommerce/uploadservice/dto/TagDto.java
@@ -2,16 +2,18 @@ package com.travelvcommerce.uploadservice.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class TagDto {
     private Long id;
-    private String tag_id;
+    private String tagId;
     private String type;
     private String content;
 
     @Data
     public static class TagResponseDto {
-        private String tag_id;
+        private String tagId;
         private String type;
         private String content;
     }
