215af0267a74c7ac4ac851405bdf79cecfc2d2fc
Hankyul Min <denirokr22@gmail.com>
2023-07-23 17:39:57 +0900

    Fix: 서버시간 한국시간으로 수정

--- src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
+++ src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
@@ -3,11 +3,17 @@ package com.travelvcommerce.uploadservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class UploadServiceApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UploadServiceApplication.class, args);
     }
-
 }
