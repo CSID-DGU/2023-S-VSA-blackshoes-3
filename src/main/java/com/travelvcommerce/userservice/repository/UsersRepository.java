a93ccf718e85c4210a8365d74b7364d6d65359b9
Subin <70842920+bin-pro@users.noreply.github.com>
2023-07-28 16:42:47 +0900

    Chore: 소셜로그인 로직 변경에 따른 UserRepository에 Provider 수정 추가

--- src/main/java/com/travelvcommerce/userservice/repository/UsersRepository.java
+++ src/main/java/com/travelvcommerce/userservice/repository/UsersRepository.java
@@ -2,6 +2,8 @@ package com.travelvcommerce.userservice.repository;
 
 import com.travelvcommerce.userservice.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +12,14 @@ import java.util.Optional;
 public interface UsersRepository extends JpaRepository<Users, String> {
     Optional<Users> findByEmail(String email);
     void deleteByUserId(String userId);
-
     boolean existsByEmail(String email);
     Optional<Users> findByUserId(String userId);
+
+    @Modifying
+    @Query("UPDATE Users u SET u.providerId = :providerId WHERE u.email = :email")
+    void updateProviderId(String providerId, String email);
+    @Modifying
+    @Query("UPDATE Users u SET u.provider = :provider WHERE u.email = :email")
+    void updateProvider(String provider, String email);
+
 }
