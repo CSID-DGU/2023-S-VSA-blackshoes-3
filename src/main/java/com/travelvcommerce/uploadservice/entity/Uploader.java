89f657c383d5ffcaac8184ab6f12c0449e53bc5d
Hankyul Min <denirokr22@gmail.com>
2023-08-03 10:48:19 +0900

    Feat: 업로더 정보 create, update, delete kafka 토픽 sub

--- src/main/java/com/travelvcommerce/uploadservice/entity/Uploader.java
+++ src/main/java/com/travelvcommerce/uploadservice/entity/Uploader.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Entity
@@ -26,4 +27,16 @@ public class Uploader implements Serializable {
 
     @OneToMany(mappedBy = "uploader", cascade = CascadeType.ALL)
     private List<Video> videos;
+
+    public static void update(Uploader uploader, Map<Object, Object> payloadMap) {
+        if (payloadMap.containsKey("sellerId")) {
+            uploader.setSellerId((String) payloadMap.get("sellerId"));
+        }
+        if (payloadMap.containsKey("sellerName")) {
+            uploader.setSellerName((String) payloadMap.get("sellerName"));
+        }
+        if (payloadMap.containsKey("sellerLogo")) {
+            uploader.setSellerLogo((byte[]) payloadMap.get("sellerLogo"));
+        }
+    }
 }
