ebc75dd2b4822200cc84d3e89c61b108baff0a54
asheroh <wnstj701@naver.com>
2023-08-03 16:57:43 +0900

    Chore: Install react video.js and setting

--- src/components/Fragments/Vupload.jsx
+++ src/components/Fragments/Vupload.jsx
@@ -12,7 +12,9 @@ import HashLoader from "react-spinners/HashLoader";
 import { faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import Plus from "../../assets/images/plus.svg";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
 const Vupload = ({
   userId,
@@ -32,6 +34,8 @@ const Vupload = ({
 
   // State-------------------------------------------------------
   const [selectedQuality, setSelectedQuality] = useState(qualities[0]);
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
 
   // Function----------------------------------------------------
   const handleQualityChange = (e) => {
@@ -53,6 +57,18 @@ const Vupload = ({
     }
   };
   console.log(`${baseUrl}/${selectedQuality}.m3u8`);
+  // ComponentDidMount-------------------------------------------
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
   return (
     <>
       <VideoInput type="file" accept="video/*" id="video-input" onChange={handleVideoChange} />
@@ -66,13 +82,13 @@ const Vupload = ({
           </VideoPreview>
         ) : preview2 ? (
           <>
-            <select id="qualitySelect" value={selectedQuality} onChange={handleQualityChange}>
+            {/* <select id="qualitySelect" value={selectedQuality} onChange={handleQualityChange}>
               {qualities.map((quality) => (
                 <option key={quality} value={quality}>
                   {quality}
                 </option>
               ))}
-            </select>
+            </select> */}
             <VideoPreview controls>
               <source
                 src={`${baseUrl}/${selectedQuality}.m3u8`}
