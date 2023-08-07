9240ba77f480f0edea277353c7dd5e0b926e0a66
Hankyul Min <denirokr22@gmail.com>
2023-08-07 22:33:07 +0900

    Chore: 빌드 오류 수정, cors 설정 수정

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:11
+
+WORKDIR /app
+COPY gradlew build.gradle settings.gradle ./
+COPY gradle ./gradle
+COPY gradle/wrapper ./gradle/wrapper
+
+RUN chmod +x ./gradlew
+RUN ./gradlew --no-daemon dependencies
+COPY src ./src
+
+RUN ./gradlew --no-daemon build -x test
+RUN mkdir -p target && (cd build/libs && cp user-service-0.0.1-SNAPSHOT.jar ../../target/user-service.jar)
+
+CMD ["java", "-jar", "target/user-service.jar"]
+EXPOSE 8001
--- src/main/java/com/travelvcommerce/userservice/config/WebConfig.java
+++ src/main/java/com/travelvcommerce/userservice/config/WebConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class WebConfigure implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("/*")    //외부에서 들어오는 모든 url 을 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")    //허용되는 Method
                 .allowedHeaders("*")    //허용되는 헤더
                 .allowCredentials(true)    //자격증명 허용
