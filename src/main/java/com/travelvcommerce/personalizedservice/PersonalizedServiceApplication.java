ce3f5272ac69e2a4bbba00637d3791f5aaf24e88
Hankyul Min <denirokr22@gmail.com>
2023-08-08 15:38:49 +0900

    Init: personalized-service 프로젝트

---
+++ src/main/java/com/travelvcommerce/personalizedservice/PersonalizedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travelvcommerce.personalizedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalizedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travelvcommerce/personalizedservice/PersonalizedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelvcommerce.personalizedservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
