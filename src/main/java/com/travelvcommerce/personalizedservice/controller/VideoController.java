e83f6d2fc58ba3aece10603a436eba30c7ddc840
Subin <70842920+bin-pro@users.noreply.github.com>
2023-08-09 11:14:28 +0900

    Chore: Entity 열 타입 수정, builder 추가

---
+++ src/main/java/com/travelvcommerce/personalizedservice/controller/VideoController.java
@@ -0,0 +1,17 @@
+package com.travelvcommerce.personalizedservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController {
+    /*
+    * sellerid로 비디오 삭제
+    * 비디오 좋아요
+    * 비디오 좋아요 삭제
+    * 비디오 좋아요 리스트
+    * 조회한 비디오 컬럼 추가, 이미 있으면 count += 1
+    *
+    * */
+
+}
--- src/main/java/com/travelvcommerce/personalizedservice/entity/LikeVideo.java
+++ src/main/java/com/travelvcommerce/personalizedservice/entity/LikeVideo.java
@@ -1,26 +1,38 @@
 package com.travelvcommerce.personalizedservice.entity;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor
 @Table(name = "Like_Videos")
 public class LikeVideo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "userId")
-    private Long userId;
+    @Column(name = "user_id")
+    private String userId;
 
-    @Column(name = "videoId")
-    private Long videoId;
+    @Column(name = "video_id")
+    private String videoId;
 
-    @Column(name = "createdAt")
-    private String createdAt;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
-    @Column(name = "sellerId")
+    @Column(name = "seller_id")
     private String sellerId;
 
+    @Builder
+    public LikeVideo(String userId, String videoId, LocalDateTime createdAt, String sellerId) {
+        this.userId = userId;
+        this.videoId = videoId;
+        this.createdAt = createdAt;
+        this.sellerId = sellerId;
+    }
 }
 /*
     * sellerId도 필요
