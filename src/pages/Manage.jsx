8553fbed4bedb6cb9b8e6c522161cfbae4211320
asheroh <wnstj701@naver.com>
2023-08-11 03:31:44 +0900

    Docs: Add Mright file

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -17,6 +17,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { SlaveInstance, UploadInstance } from "../api/axios";
 import Mleft from "../components/Fragments/Manage/Mleft";
 import Mmiddle from "../components/Fragments/Manage/Mmiddle";
+import Mright from "../components/Fragments/Manage/Mright";
 
 const Manage = () => {
   // Constant----------------------------------------------------
@@ -146,79 +147,7 @@ const Manage = () => {
             themeTag={themeTag}
           />
         </M.LeftMiddleBox>
-        <M.RightBox>
-          <U.TitleLeftBox>
-            <U.SpanTitle>광고수정</U.SpanTitle>
-            <U.AdUploadButton>
-              <U.FullIcon src={PlusButton} alt="plus-button" loading="lazy" />
-            </U.AdUploadButton>
-          </U.TitleLeftBox>
-          <M.AdModifySection>
-            <U.AdInputSection>
-              <U.TimeBox>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DemoContainer components={["TimeField"]}>
-                    <TimeField
-                      label="시작 시간"
-                      onChange={(e) => {
-                        const receivedTime = new Date(e.$d);
-                        const hours = receivedTime.getHours();
-                        const minutes = receivedTime.getMinutes();
-                        const seconds = receivedTime.getSeconds();
-                        const totalMilliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000;
-                        // setStartTime(totalMilliseconds.toString());
-                      }}
-                      format="HH:mm:ss"
-                      color="success"
-                    />
-                    <TimeField
-                      label="종료 시간"
-                      onChange={(e) => {
-                        const receivedTime = new Date(e.$d);
-                        const hours = receivedTime.getHours();
-                        const minutes = receivedTime.getMinutes();
-                        const seconds = receivedTime.getSeconds();
-                        const totalMilliseconds = (hours * 3600 + minutes * 60 + seconds) * 1000;
-                        // setEndTime(totalMilliseconds.toString());
-                      }}
-                      format="HH:mm:ss"
-                      color="success"
-                    />
-                  </DemoContainer>
-                </LocalizationProvider>
-              </U.TimeBox>
-              <U.ContentBox>
-                <U.NormalSpan>내용</U.NormalSpan>
-                <U.AdInput
-                  type="text"
-                  placeholder="수정할 광고 내용을 입력해주세요."
-                  width="85%"
-                  height="100px"
-                  // onChange={(e) => setAdContent(e.target.value)}
-                />
-              </U.ContentBox>
-              <U.LinkBox>
-                <U.NormalSpan>링크</U.NormalSpan>
-                <U.AdInput
-                  type="text"
-                  placeholder="수정할 광고 링크를 첨부해주세요."
-                  width="85%"
-                  height="35px"
-                  // onChange={(e) => setAdUrl(e.target.value)}
-                />
-              </U.LinkBox>
-              <U.RemoveButton>
-                <U.SmallImage src={Minus} alt="minus" />
-              </U.RemoveButton>
-            </U.AdInputSection>
-          </M.AdModifySection>
-          <U.TitleLeftBox>
-            <U.SpanTitle>댓글조회</U.SpanTitle>
-          </U.TitleLeftBox>
-          <M.AdCommentEmptySection>
-            <M.BoldSpan>영상 선택 후 댓글을 조회할 수 있습니다.</M.BoldSpan>
-          </M.AdCommentEmptySection>
-        </M.RightBox>
+        <Mright />
       </Body>
     </GridWrapper>
   );
