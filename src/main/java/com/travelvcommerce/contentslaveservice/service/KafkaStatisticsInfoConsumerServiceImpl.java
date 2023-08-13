9e8967179f7f3ab002cbe2d71000a8e97449792d
Hankyul Min <denirokr22@gmail.com>
2023-08-13 19:44:34 +0900

    Fix: 통계 정보 동기화 메소드 @Transactional 누락 및 save() 누락 수정

--- src/main/java/com/travelvcommerce/contentslaveservice/service/KafkaStatisticsInfoConsumerServiceImpl.java
+++ src/main/java/com/travelvcommerce/contentslaveservice/service/KafkaStatisticsInfoConsumerServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -19,6 +20,7 @@ public class KafkaStatisticsInfoConsumerServiceImpl implements KafkaStatisticsIn
 
     @Override
     @KafkaListener(topics = "statistics-update")
+    @Transactional
     public void updateStatistics(String payload) {
         log.info("received payload='{}'", payload);
         StatisticsInfoDto statisticsInfoDto;
@@ -33,16 +35,19 @@ public class KafkaStatisticsInfoConsumerServiceImpl implements KafkaStatisticsIn
         String videoId = statisticsInfoDto.getVideoId();
         Video video = videoRepository.findByByVideoId(videoId).orElseThrow(() -> new RuntimeException("video not found"));
 
-        if (statisticsInfoDto.getLikes() != 0) {
+        if (statisticsInfoDto.getLikes() != -1) {
             video.updateLikes(statisticsInfoDto.getLikes());
+            videoRepository.save(video);
             return;
         }
-        if (statisticsInfoDto.getViews() != 0) {
+        if (statisticsInfoDto.getViews() != -1) {
             video.updateViews(statisticsInfoDto.getViews());
+            videoRepository.save(video);
             return;
         }
-        if (statisticsInfoDto.getAdClicks() != 0) {
+        if (statisticsInfoDto.getAdClicks() != -1) {
             video.updateAdClicks(statisticsInfoDto.getAdClicks());
+            videoRepository.save(video);
             return;
         }
     }
