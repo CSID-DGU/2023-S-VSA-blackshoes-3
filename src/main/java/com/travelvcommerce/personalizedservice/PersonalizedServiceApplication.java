70804ca07aa2c5fae30b3924856f267a1f81be0a
Hankyul Min <denirokr22@gmail.com>
2023-08-16 12:20:11 +0900

    Feat: Eureka Discovery 적용

--- src/main/java/com/travelvcommerce/personalizedservice/PersonalizedServiceApplication.java
+++ src/main/java/com/travelvcommerce/personalizedservice/PersonalizedServiceApplication.java
@@ -2,9 +2,18 @@ package com.travelvcommerce.personalizedservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PersonalizedServiceApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PersonalizedServiceApplication.class, args);
