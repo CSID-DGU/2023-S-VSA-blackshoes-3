06b327159da8912965a22b5c1749b576582bd271
Hankyul Min <denirokr22@gmail.com>
2023-08-16 12:18:16 +0900

    Feat: Eureka Discovery 적용

--- src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
+++ src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
@@ -2,11 +2,13 @@ package com.travelvcommerce.uploadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UploadServiceApplication {
     @PostConstruct
     void started(){
