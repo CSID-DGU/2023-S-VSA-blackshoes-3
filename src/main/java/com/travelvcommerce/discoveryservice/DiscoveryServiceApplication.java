afd072292a1fdc4b167fc3800773d0b743274bfa
Hankyul Min <denirokr22@gmail.com>
2023-08-16 12:24:18 +0900

    Feat: discovery-service

---
+++ src/main/java/com/travelvcommerce/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,22 @@
+package com.travelvcommerce.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
