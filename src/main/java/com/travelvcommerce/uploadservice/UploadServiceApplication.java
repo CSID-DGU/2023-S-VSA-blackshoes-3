25d60da6cd6015e5908d470ec971a6b0ff279fa3
Hankyul Min <denirokr22@gmail.com>
2023-08-16 20:38:50 +0900

    Feat: 학교 서버 사용 위한 리다이렉트 설정

--- src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
+++ src/main/java/com/travelvcommerce/uploadservice/UploadServiceApplication.java
@@ -1,8 +1,12 @@
 package com.travelvcommerce.uploadservice;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -13,6 +17,20 @@ public class UploadServiceApplication {
     @PostConstruct
     void started(){
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcatServletWebServerFactory;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        connector.setRedirectPort(9127);
+        return connector;
     }
 
     public static void main(String[] args) {
