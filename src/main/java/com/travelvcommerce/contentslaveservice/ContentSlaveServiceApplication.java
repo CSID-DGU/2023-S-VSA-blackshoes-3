6de85bf1e1f6505b2d61b98866f16f6c2ad7f888
Hankyul Min <denirokr22@gmail.com>
2023-08-16 12:17:51 +0900

    Feat: Eureka Discovery 적용

--- src/main/java/com/travelvcommerce/contentslaveservice/ContentSlaveServiceApplication.java
+++ src/main/java/com/travelvcommerce/contentslaveservice/ContentSlaveServiceApplication.java
@@ -2,9 +2,18 @@ package com.travelvcommerce.contentslaveservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContentSlaveServiceApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ContentSlaveServiceApplication.class, args);
